05a693f23805630a1bbb7a3f84b92827632a8ffd
Valifer13 <vianviszello@gmail.com>
2024-11-27 10:23:35 +0800

    update routes for homepage

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $menu = json_decode(file_get_contents(storage_path('data/data.json')), true);
+    return view('pages.home', ['data' => $menu]);
+})->name('home');
