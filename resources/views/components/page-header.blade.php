7a574507da923314101ad8b7c7372309ebd4b99b
Valifer13 <vianviszello@gmail.com>
2024-11-30 12:32:51 +0800

    add attribute on page-header components

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -5,19 +5,23 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="text-center wow fadeInUp">
-                    <h5 class="uptitle">Discover</h5>
+                    <h5 class="uptitle">{{ empty($subtitle) ?  "Discover" : $subtitle }}</h5>
                     <h2>{{ $title }}</h2>
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item "><a href="/">Home</a></li>
                         @php
                             $segments = Request::segments();
                             $currentPath = '/';
+                            $lastSegment = end($segments);
                         @endphp
                         @foreach ($segments as $segment)
                             @php
                                 $currentPath .= $segment . '/';
+                                $isActive = $segment === $lastSegment;
                             @endphp
-                            <li class="breadcrumb-item active" aria-current="page"><a href="{{ url($currentPath) }}">{{ ucfirst(str_replace('-', '', $segment)) }}</a></li>
+                            <li class="breadcrumb-item {{ $isActive ? 'active' : ''}}" {{ $isActive ? 'aria-current="page"' : '' }}>
+                                <a href="{{ url($currentPath) }}">{{ ucfirst(str_replace('-', '', $segment)) }}</a>
+                            </li>
                         @endforeach
                     </ol>
                 </div>
